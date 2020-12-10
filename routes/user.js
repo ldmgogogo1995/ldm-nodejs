f05025009e52fdb317e5c0c685ec65d3fa1af201
luodaming <775495877@qq.com>
2020-12-10 17:27:26 +0800

    feat

--- routes/user.js
+++ routes/user.js
@@ -2,5 +2,18 @@ const express = require('express')
 const router = express.Router()
 router.get('/list', (req, res) => {
   console.log('请求用户列表')
+  res.send({ code: 200, data: {}, message: '查看用户成功' })
+})
+router.post('/add', (req, res) => {
+  console.log('添加用户')
+  res.send({ code: 200, data: 0, message: '添加用户成功' })
+})
+router.put('/edit', (req, res) => {
+  console.log('修改用户')
+  res.send({ code: 200, data: 0, message: '修改用户成功' })
+})
+router.delete('/del', (req, res) => {
+  console.log('删除用户')
+  res.send({ code: 200, data: 0, message: '删除用户成功' })
 })
 module.exports = router
