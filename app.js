48730083226cab5722ed3d84f70e4bf8ebd3f358
luodaming <775495877@qq.com>
2020-12-10 15:18:50 +0800

    feat

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-
-
+const userRoute = require(__dirname + '/routes/user')
+app.use('/user',userRoute);
 
 
 
--- routes/user.js
+++ routes/user.js
@@ -1,2 +1,6 @@
 const express = require('express')
 const router = express.Router()
+router.get('/list', (req, res) => {
+  console.log('请求用户列表')
+})
+module.exports = router
